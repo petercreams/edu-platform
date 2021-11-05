bebcc10352933884d4b61a4362bada5d568453c8
Piotr Smietanka <piotrsm99@gmail.com>
2021-11-05 12:03:31 +0100

    simplify component

--- components/Home/Backdrop.js
+++ components/Home/Backdrop.js
@@ -1,17 +1,5 @@
-import styles from "./Backdrop.module.scss"
+import styles from "./Backdrop.module.scss";
 
 export default function Backdrop({ children }) {
-  return (
-    <div>
-      <div className={styles.container}>
-        <div className={styles.gridContainer}>
-          <hr className={styles.marginLeft}></hr>
-
-          {children}
-
-          <hr className={styles.marginRight}></hr>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className={styles.container}>{children}</div>;
 }
