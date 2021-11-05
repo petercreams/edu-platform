566d90535ba8087cc768f884921ece63d14e0457
Piotr Smietanka <piotrsm99@gmail.com>
2021-11-05 14:57:50 +0100

    style about section

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
 import Backdrop from "../components/Home/Backdrop";
+import CardInfo from "../components/Home/CardInfo";
 import Navbar from "../components/Home/Navbar";
+import PaperContainer from "../components/Home/PaperContainer";
+import TitleCard from "../components/Home/TitleCard";
 import Top from "../components/Home/Top";
-import styles from "../styles/index.module.css";
+
+import styles from "../styles/index.module.scss";
 
 export default function Home() {
   return (
@@ -17,9 +21,25 @@ export default function Home() {
       <main>
         <Top />
         <Backdrop>
-          <div className={styles.title1}>
-            <p className={styles.hello}>How it works?</p>
-          </div>
+          <PaperContainer>
+            <section className={styles.about}>
+              <div className={styles.title1_container}>
+                <TitleCard title={"How it works?"}/>
+              </div>
+              <div className={styles.cardInfo_container}>
+                <CardInfo
+                  title={"1."}
+                  text={"Check our offer and choose your course"}
+                />
+                <CardInfo title={"2."} text={"Call us or send us the form"} />
+                <CardInfo
+                  title={"3."}
+                  text={"Enjoy a course with your teacher"}
+                />
+              </div>
+            </section>
+          </PaperContainer>
+
         </Backdrop>
       </main>
     </div>
