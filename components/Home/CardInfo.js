20107d1002b0792ebbfd13d98cb9f1ad33784f4e
Piotr Smietanka <piotrsm99@gmail.com>
2021-11-06 12:18:07 +0100

    complete component styling

--- components/Home/CardInfo.js
+++ components/Home/CardInfo.js
@@ -1,11 +1,23 @@
 import styles from "./CardInfo.module.scss";
+import { useState, useEffect } from "react";
 
 export default function CardInfo(props) {
+  const [isCourse, setIsCourse] = useState(false);
+
+  // render different font-size if courses card
+  useEffect(() => {
+    props.design == "courses" && setIsCourse(true);
+  }, []);
+
   return (
     <div>
       <div className={styles.card_container}>
-        <h3>{props.title}</h3>
+        <div className={styles.title_container}>
+          <h3 style={isCourse ? { fontSize: "3.5rem" } : null}>{props.title}</h3>
+        </div>
+        <div className={styles.text_container}>
         <p>{props.text}</p>
+        </div>
       </div>
     </div>
   );
