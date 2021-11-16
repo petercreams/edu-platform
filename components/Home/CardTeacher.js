d537597151f9ccfaf9ff8933c419ac8fa38119e7
Piotr Smietanka <piotrsm99@gmail.com>
2021-11-16 11:20:36 +0100

    adjust styling

--- components/Home/CardTeacher.js
+++ components/Home/CardTeacher.js
@@ -11,35 +11,38 @@ export default function CardTeacher(props) {
 
   return (
     <div className={styles.card_container}>
-      <div
-        style={isNormal ? { order: "0" } : { order: "1" }}
-        className={styles.img_container}
-      >
-        <img src={props.img}></img>
-      </div>
-      <div
-        style={isNormal ? { order: "1" } : { order: "0" }}
-        className={styles.text_container}
-      >
-        <div className={styles.name_container}>
-          <h2>{props.name}</h2>
-        </div>
-        <div className={styles.description_container}>
-          <ul>
-            <li>{props.info1}</li>
-            <li>{props.info2}</li>
-            <li>{props.info3}</li>
-          </ul>
+      <div className={styles.top_card} />
+      <div className={styles.bottom_card}>
+        <div
+          style={isNormal ? { order: "0" } : { order: "1" }}
+          className={styles.img_container}
+        >
+          <img src={props.img}></img>
         </div>
         <div
-          style={
-            isNormal
-              ? { justifyContent: "flex-end" }
-              : { justifyContent: "flex-start" }
-          }
-          className={styles.skills_container}
+          style={isNormal ? { order: "1" } : { order: "0" }}
+          className={styles.text_container}
         >
-          <h5>{props.skills}</h5>
+          <div className={styles.name_container}>
+            <h2>{props.name}</h2>
+          </div>
+          <div className={styles.description_container}>
+            <ul>
+              <li>{props.info1}</li>
+              <li>{props.info2}</li>
+              <li>{props.info3}</li>
+            </ul>
+          </div>
+          <div
+            style={
+              isNormal
+                ? { justifyContent: "flex-end" }
+                : { justifyContent: "flex-start" }
+            }
+            className={styles.skills_container}
+          >
+            <h5>{props.skills}</h5>
+          </div>
         </div>
       </div>
     </div>
