03b1b64bd74fc33b40f4b72237fb204e309a7e9a
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-01 10:33:33 +0100

    feat: add logged in view

--- components/Home/Navbar.js
+++ components/Home/Navbar.js
@@ -1,8 +1,30 @@
 import styles from "./Navbar.module.scss";
+import { useState, useEffect } from "react";
 
 import Link from "next/link";
 
 export default function Navbar(params) {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const clickHandler = (e) => {
+    if (!isClicked) {
+      setIsClicked(true);
+    } else {
+      setIsClicked(false);
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log(isClicked);
+  //   if (isClicked) {
+  //     setTimeout(() => {
+  //       setIsClicked(false);
+  //       console.log(isClicked);
+  //     }, [5000]);
+  //   }
+  // }, [isClicked]);
+
   return (
     <nav>
       <div className={styles.navbar_backdrop}>
@@ -45,11 +67,47 @@ export default function Navbar(params) {
               </li>
             </ul>
 
-            <div className={styles.button_container}>
-              <form action="/user/login">
-                <button>Sign In</button>
-              </form>
-            </div>
+            {isLoggedIn ? (
+              <div className={styles.profile_container}>
+                {/* {should be taken from server} */}
+                <img
+                  onClick={clickHandler}
+                  id={styles.profile_img}
+                  src="/users/user.jpg"
+                />
+                <img
+                  onClick={clickHandler}
+                  id={styles.more}
+                  src="/icons/more.svg"
+                />
+
+                {isClicked && (
+                  <div className={styles.profile_modal}>
+                    <Link href="/user/account">
+                      <a style={{ borderBottom: "1px solid #000000" }}>
+                        My account
+                      </a>
+                    </Link>
+
+                  <Link href="/user/courses">
+                  <a style={{ borderBottom: "1px solid #000000" }}>
+                      My courses
+                    </a>
+                  </Link>
+                    
+                    <a id={styles.logout}>Log Out</a>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div className={styles.button_container}>
+                <Link href="/user/login">
+                  <a>
+                    <button>Sign In</button>
+                  </a>
+                </Link>
+              </div>
+            )}
           </div>
         </div>
       </div>
