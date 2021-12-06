a53e13b009832945ad586ea59564926f1c6f318d
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-06 15:32:53 +0100

    feat: add initial filter & sort logic

--- pages/user/courses/[courseId].js
+++ pages/user/courses/[courseId].js
@@ -4,23 +4,36 @@ import UserPanel from "../../../components/User/UserPanel";
 import styles from "../../../styles/courses/[courseId].module.scss";
 import { useState } from "react";
 
-const sections = [];
+const sections = ["1", "2", "3", "4", "5"];
 const sort = ["Asc", "Desc"];
 
 export default function courseId(params) {
+  const [filterValue, setFilterValue] = useState("1");
+  const [sortValue, setSortValue] = useState("Asc");
 
-    const [filterClicked, setFilterClicked] = useState(false);
-    const [sortClicked, setSortClicked] = useState(false)
+  const [filterClicked, setFilterClicked] = useState(false);
+  const [sortClicked, setSortClicked] = useState(false);
 
   const filterHandler = () => {
-    console.log("filter clicked");
-    setFilterClicked(!filterClicked);
+    setFilterClicked(true);
   };
 
   const sortHandler = () => {
     console.log("sort clicked");
 
     setSortClicked(true);
+  };
+
+  const selectFilter = (e) => {
+    let value = e.target.innerText;
+    setFilterValue(value);
+    setFilterClicked(false);
+  };
+
+  const selectSort = (e) => {
+    let value = e.target.innerText;
+    setSortValue(value);
+    setSortClicked(false);
   };
 
   return (
@@ -34,12 +47,33 @@ export default function courseId(params) {
           <div className={styles.options_container}>
             <div className={styles.options__item}>
               <p>Filter by section:</p>
-              <span onClick={filterHandler}>1</span>
-              {filterClicked && <div>xD</div>}
+              <span onClick={filterHandler}>{filterValue}</span>
+              {filterClicked && (
+                <div className={styles.modal_container}>
+                  <ul>
+                    {sections.map((section) => {
+                      return (
+                        <li onClick={(e) => selectFilter(e)}>{section}</li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              )}
             </div>
             <div className={styles.options__item}>
               <p>Sort by lesson:</p>
-              <span onClick={sortHandler}>Asc</span>
+              <span onClick={sortHandler}>{sortValue}</span>
+              {sortClicked && (
+                <div className={styles.modal_container}>
+                  <ul>
+                    {sort.map((sortElement) => {
+                      return (
+                        <li onClick={(e) => selectSort(e)}>{sortElement}</li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              )}
             </div>
           </div>
 
