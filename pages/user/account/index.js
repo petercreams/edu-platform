57ae617502d23543b23338c40bead5cdc63fa9b5
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-06 14:54:32 +0100

    edit: change navbar display mode

--- pages/user/account/index.js
+++ pages/user/account/index.js
@@ -1,12 +1,14 @@
 import Navbar from "../../../components/Home/Navbar";
-import AccountPage from "../../../components/User/AccountPage";
+import AccountPage from "../../../components/User/AccountPageTest";
 import styles from "../../../styles/user/account.module.scss";
 
 export default function Account(props, { children }) {
+  console.log("Account loaded");
+
   return (
     <>
       <div className={styles.account_container}>
-        <Navbar />
+        <Navbar mode={"account"} />
         <AccountPage />
       </div>
     </>
