a6d501d87aa557f4fe51c303fba269058a9bc5bd
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-06 14:54:01 +0100

    edit: Courses display

--- components/User/CoursesPage.js
+++ components/User/CoursesPage.js
@@ -4,10 +4,14 @@ import Link from "next/dist/client/link";
 import { useState, useEffect } from "react";
 import UserPanel from "./UserPanel";
 import CourseCard from "./CourseCard";
+import Courses from "./Courses";
+
 
 export default function CoursesPage(params) {
   const [isClicked, setIsClicked] = useState(false);
   const [isLogOut, setIsLogOut] = useState(false);
+
+  // const [courses, setCourses] = useState([]);
 
   return (
     <div className={styles.menu_container}>
@@ -15,20 +19,7 @@ export default function CoursesPage(params) {
       <div className={styles.right_bar}>
         <h1>My Courses</h1>
         <div className={styles.courses_container}>
-          <CourseCard
-            key="1"
-            id="1"
-            img="/courses/newtone.png"
-            name="Math for 8th grades"
-            author="NewTone"
-          />
-          <CourseCard
-            key="2"
-            id="2"
-            img="/courses/newtone.png"
-            name="Math for Matura Exam"
-            author="NewTone"
-          />
+          <Courses />
         </div>
       </div>
     </div>
