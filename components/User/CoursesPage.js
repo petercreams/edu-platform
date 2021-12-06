ac2c56121ba3239c00f75a24d6533e5efe04447b
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-06 11:51:51 +0100

    feat: add courses page

--- components/User/CoursesPage.js
+++ components/User/CoursesPage.js
@@ -1,37 +1,34 @@
-import Navbar from "../Home/Navbar";
 import styles from "./CoursesPage.module.scss";
-import CardOption from "./CardOption";
 import Link from "next/dist/client/link";
 
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import UserPanel from "./UserPanel";
+import CourseCard from "./CourseCard";
 
-export default function AccountPage(params) {
-
-    const [isClicked, setIsClicked] = useState(false);
-    const [isLogOut, setIsLogOut] = useState(false);
+export default function CoursesPage(params) {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isLogOut, setIsLogOut] = useState(false);
 
   return (
     <div className={styles.menu_container}>
-      <div className={styles.left_bar}>
-        <img src="/teachers/teacher1.jpg" />
-        <h1>Piotr Śmietanka</h1>
-        
-        <Link href="/user/account">
-        <a id={styles.account}>My Account</a>
-        </Link>
-        <Link href="/user/courses">
-        <a id={styles.courses}>My Courses</a>
-        </Link>
-        
-        <a id={styles.logout}>Log Out</a>
-      </div>
+      <UserPanel option="courses" />
       <div className={styles.right_bar}>
         <h1>My Courses</h1>
-        <div className={styles.options_container}>
-          {/* <CardOption icon={"/icons/id-card.png"} text={"Change name"}/>
-          <CardOption icon={"/icons/password.png"} text={"Change password"}/>
-          <CardOption icon={"/icons/email.png"} text={"Change email"}/>
-          <CardOption icon={"/icons/photo.png"} text={"Change photo"}/> */}
+        <div className={styles.courses_container}>
+          <CourseCard
+            key="1"
+            id="1"
+            img="/courses/newtone.png"
+            name="Math for 8th grades"
+            author="NewTone"
+          />
+          <CourseCard
+            key="2"
+            id="2"
+            img="/courses/newtone.png"
+            name="Math for Matura Exam"
+            author="NewTone"
+          />
         </div>
       </div>
     </div>
