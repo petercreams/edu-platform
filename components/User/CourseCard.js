efb459050c0da08bb625e5b66b7bab51de2e390f
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-06 14:53:22 +0100

    edit: destructure props

--- components/User/CourseCard.js
+++ components/User/CourseCard.js
@@ -2,18 +2,19 @@ import styles from "./CourseCard.module.scss";
 
 import { useRouter } from 'next/router'
 
-export default function CourseCard(props) {
+export default function CourseCard({id, img, name, author}) {
     const router = useRouter()
 
   return (
-    <div onClick={() => router.push(`/user/courses/${props.id}`)} className={styles.course__item}>
+    <div onClick={() => router.push(`/user/courses/${id}`)} className={styles.course__item}>
       <div className={styles.img_container}>
-        <img src={props.img} />
+        <img src={img} />
       </div>
       <div className={styles.title_container}>
-        <p>{props.name}</p>
-        <p id={styles.author}>By {props.author}</p>
+        <p>{name}</p>
+        <p id={styles.author}>By {author}</p>
       </div>
     </div>
   );
 }
+
