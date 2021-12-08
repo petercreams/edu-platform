db2519a064fe55bb0d43f6d008139bffa02ebec3
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-09 00:22:08 +0100

    feat: add change name page

---
+++ pages/user/account/edit/name.js
@@ -0,0 +1,56 @@
+import styles from "../../../../styles/user/edit/email.module.scss";
+import Link from "next/dist/client/link";
+
+import { useState, useEffect, useRef } from "react";
+import UserPanel from "../../../../components/User/UserPanel";
+import Navbar from "../../../../components/Home/Navbar";
+
+export default function ChangeName(params) {
+
+  const newName = useRef();
+
+
+  const sendForm = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      newName: newName.current.value,
+    };
+
+    console.log("Form sent");
+
+    console.log(formData);
+  };
+
+  return (
+    <div className={styles.account_container}>
+      <Navbar mode={"account"} />
+      <div className={styles.menu_container}>
+        <UserPanel option="account" />
+        <div className={styles.right_bar}>
+          <h1>Change Name</h1>
+          <p>Edit your displayed name or cancel an action.</p>
+          <div className={styles.form_container}>
+            <form>
+              <input ref={newName} placeholder="yourname" type="text"></input>
+
+              <div className={styles.buttons_container}>
+                <Link href="/user/account">
+                  <a>
+                    <button>Cancel</button>
+                  </a>
+                </Link>
+
+                <a>
+                  <button onClick={sendForm} id={styles.long_button}>
+                    Save changes
+                  </button>
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
