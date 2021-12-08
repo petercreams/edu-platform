ee9f0c2da29783eeddcee495927442e45bea1c50
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-09 00:21:53 +0100

    feat: add read form data

--- pages/user/account/edit/email.js
+++ pages/user/account/edit/email.js
@@ -6,9 +6,18 @@ import UserPanel from "../../../../components/User/UserPanel";
 import Navbar from "../../../../components/Home/Navbar";
 
 export default function ChangeEmail(params) {
+  const newEmail = useRef();
+
   const sendForm = (e) => {
     // e.preventDefault();
+
+    const formData = {
+      newEmail: newEmail.current.value,
+    };
+
     console.log("Form sent");
+
+    console.log(formData);
   };
 
   return (
@@ -18,10 +27,14 @@ export default function ChangeEmail(params) {
         <UserPanel option="account" />
         <div className={styles.right_bar}>
           <h1>Change Email</h1>
-          <p>Edit your displayed name or cancel an action.</p>
-            <div className={styles.form_container}>
+          <p>Edit your displayed email or cancel an action.</p>
+          <div className={styles.form_container}>
             <form>
-              <input placeholder="youremail@adress.com" type="email"></input>
+              <input
+                ref={newEmail}
+                placeholder="youremail@adress.com"
+                type="email"
+              ></input>
 
               <div className={styles.buttons_container}>
                 <Link href="/user/account">
@@ -37,10 +50,7 @@ export default function ChangeEmail(params) {
                 </a>
               </div>
             </form>
-
-            </div>
-            
-          
+          </div>
         </div>
       </div>
     </div>
