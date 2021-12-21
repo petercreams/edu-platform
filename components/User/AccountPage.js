c5ee682624bb180fd1f1439cd3618c881b4ce12e
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-21 18:26:21 +0100

    style: code

--- components/User/AccountPage.js
+++ components/User/AccountPage.js
@@ -1,30 +1,53 @@
-import Navbar from "../Home/Navbar";
 import styles from "./AccountPage.module.scss";
 import CardOption from "./CardOption";
-import Link from "next/dist/client/link";
 
-import {useState, useEffect} from "react";
+import { useState } from "react";
 import UserPanel from "./UserPanel";
 
 export default function AccountPage(params) {
+  const [isClicked, setIsClicked] = useState(false);
+  const [isLogOut, setIsLogOut] = useState(false);
 
-    const [isClicked, setIsClicked] = useState(false);
-    const [isMyAccount, setIsMyAccount] = useState(true);
-    const [isMyCourses, setisMyCourses] = useState(false);
-    const [isLogOut, setIsLogOut] = useState(false);
+  const accountPanel = (
+    <div className={styles.right_bar}>
+      <h1>My Account</h1>
+      <div className={styles.options_container}>
+        <div className={styles.options_item}>
+          <CardOption
+            icon={"/icons/id-card.png"}
+            text={"Change name"}
+            link={"/user/account/edit/name"}
+          />
+        </div>
+        <div className={styles.options_item}>
+          <CardOption
+            icon={"/icons/password.png"}
+            text={"Change password"}
+            link={"/user/account/edit/password"}
+          />
+        </div>
+        <div className={styles.options_item}>
+          <CardOption
+            icon={"/icons/email.png"}
+            text={"Change email"}
+            link={"/user/account/edit/email"}
+          />
+        </div>
+        <div className={styles.options_item}>
+          <CardOption
+            icon={"/icons/photo.png"}
+            text={"Change photo"}
+            link={"/user/account/edit/photo"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 
   return (
     <div className={styles.menu_container}>
       <UserPanel />
-      <div className={styles.right_bar}>
-        <h1>My Account</h1>
-        <div className={styles.options_container}>
-          <CardOption icon={"/icons/id-card.png"} text={"Change name"} link={"/user/account/edit/name"}/>
-          <CardOption icon={"/icons/password.png"} text={"Change password"} link={"/user/account/edit/password"}/>
-          <CardOption icon={"/icons/email.png"} text={"Change email"} link={"/user/account/edit/email"}/>
-          <CardOption icon={"/icons/photo.png"} text={"Change photo"} link={"/user/account/edit/photo"}/>
-        </div>
-      </div>
+      {accountPanel}
     </div>
   );
 }
