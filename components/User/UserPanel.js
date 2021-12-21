5034fb43452addb43f03d12a828e892f2e1befe3
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-21 18:12:17 +0100

    feat: add user Context

--- components/User/UserPanel.js
+++ components/User/UserPanel.js
@@ -1,19 +1,46 @@
 import styles from "./UserPanel.module.scss";
-import Link from "next/dist/client/link";
+import { useRouter } from "next/router";
+import { useAuthProvider } from "../../firebase/AuthProvider";
+import { useUser } from "../../firebase/useUserNode";
+import { useEffect, useState } from "react";
+
+import { withProtected } from "../../firebase/routes";
 
 export default function UserPanel(props) {
+  var option = props.option;
+
+  const router = useRouter();
+
+  const user = useAuthProvider();
+
+  const { logout } = useUser();
+
+  const signOut = () => {
+    logout();
+  };
+
   return (
     <div className={styles.userPanel_container}>
-        <img src="/teachers/teacher1.jpg" />
-        <h1>Piotr Śmietanka</h1>
-        <Link href="/user/account">
-        <a id={styles.account}>My Account</a>
-        </Link>
-        <Link href="/user/courses">
-        <a id={styles.courses}>My Courses</a>
-        </Link>
-        
-        <a id={styles.logout}>Log Out</a>
-      </div>
+      <img src={user?.photoURL ? user.photoURL : "/icons/default_photo.jpg"} />
+      <h1>{user?.displayName ? user.displayName : "nickname"}</h1>
+
+      <p
+        onClick={() => router.push("/user/account")}
+        id={option == "courses" ? styles.account_off : styles.account_on}
+      >
+        My Account
+      </p>
+
+      <p
+        onClick={() => router.push("/user/courses")}
+        id={option == "courses" ? styles.courses_on : styles.courses_off}
+      >
+        My Courses
+      </p>
+
+      <p onClick={signOut} id={styles.logout}>
+        Log Out
+      </p>
+    </div>
   );
 }
