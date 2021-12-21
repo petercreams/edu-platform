cca352bccff435ae7a1f2c827dccda4f77e5f1dc
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-21 18:18:12 +0100

    feat: add auth context parent

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/global.scss";
+import { useUser } from "../firebase/useUserNode";
+import { useState, useEffect } from "react";
+import { AuthProvider } from "../firebase/AuthProvider";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
