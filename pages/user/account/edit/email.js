08ab9bf7813e6a04a3d83b30f69d17bee96ebbd6
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-29 10:26:09 +0100

    add: firebase support

--- pages/user/account/edit/email.js
+++ pages/user/account/edit/email.js
@@ -6,20 +6,26 @@ import UserPanel from "../../../../components/User/UserPanel";
 import Navbar from "../../../../components/Home/Navbar";
 import { useAuthProvider } from "../../../../firebase/AuthProvider";
 
+import {
+  getAuth,
+  updateEmail,
+} from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
 export default function ChangeEmail(params) {
   const newEmail = useRef();
-  const user = useAuthProvider();
+  // const user = useAuthProvider();
+
+  const [user, loading, error] = useAuthState(getAuth());
+
+  const router = useRouter();
 
   const sendForm = (e) => {
-    // e.preventDefault();
+    e.preventDefault();
 
-    const formData = {
-      newEmail: newEmail.current.value,
-    };
-
-    console.log("Form sent");
-
-    console.log(formData);
+    updateEmail(user, newEmail.current.value)
+      .then(() => router.push("/user/account"))
+      .catch((error) => console.log(error));
   };
 
   return (
--- pages/user/account/edit/name.js
+++ pages/user/account/edit/name.js
@@ -5,22 +5,27 @@ import { useState, useEffect, useRef } from "react";
 import UserPanel from "../../../../components/User/UserPanel";
 import Navbar from "../../../../components/Home/Navbar";
 import { useAuthProvider } from "../../../../firebase/AuthProvider";
+import axiosInstance from "../../../../firebase/axiosInstance";
+import axios from "axios";
+import { useUser } from "../../../../firebase/useUserNode";
+
+import { getAuth, updateProfile } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
 
 export default function ChangeName(params) {
+  const newName = useRef()
+  
+  const router = useRouter();
 
-  const newName = useRef();
-  const user = useAuthProvider();
+  const [user, loading, error] = useAuthState(getAuth());
 
   const sendForm = (e) => {
     e.preventDefault();
 
-    const formData = {
-      newName: newName.current.value,
-    };
-
-    console.log("Form sent");
-
-    console.log(formData);
+    updateProfile(user, { displayName: newName.current.value })
+      .then(() => router.push("/user/account"))
+      .catch((error) => console.log(error));
   };
 
   return (
@@ -33,7 +38,11 @@ export default function ChangeName(params) {
           <p>Edit your displayed name or cancel an action.</p>
           <div className={styles.form_container}>
             <form>
-              <input ref={newName} placeholder={user?.displayName} type="text"></input>
+              <input
+                ref={newName}
+                placeholder={user?.displayName}
+                type="text"
+              ></input>
 
               <div className={styles.buttons_container}>
                 <Link href="/user/account">
