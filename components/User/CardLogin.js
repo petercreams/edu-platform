ec99d3bde44316570c3ee4fd928732f71941c294
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-29 10:32:35 +0100

    add: firebase support

--- components/User/CardLogin.js
+++ components/User/CardLogin.js
@@ -1,14 +1,54 @@
 import styles from "./CardLogin.module.scss";
 import { useState, useRef, useEffect } from "react";
 import Link from "next/dist/client/link";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+import { auth } from "../../firebase-client/clientApp";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Login(props) {
   const [submit, setSubmit] = useState(false);
   const [errorBlank, setErrorBlank] = useState(false);
   const [errorPassword, setErrorPassword] = useState(false);
+  const [serverResponse, setServerResponse] = useState("");
 
   const userEmail = useRef();
   const userPassword = useRef();
+
+  const router = useRouter();
+
+  // Send data to the endpoint
+  const postData = async (email, password) => {
+    // const data = { email: email, password: password };
+
+    // axios
+    //   .post(process.env.NEXT_PUBLIC_SERVER_HOST + "/user/login", data)
+    //   .then((response) => {
+    //     if (response.data.error) {
+    //       console.log(response.data.error);
+    //       setServerResponse(response.data.error);
+    //     } else {
+    //       // console.log(response.data);
+
+    //       // get all user's data
+    //       const { displayName, email, photoURL, uid } =
+    //         response.data.userCredential.user;
+    //       const jwt = response.data.token;
+    //       localStorage.setItem("JWT", jwt);
+    //       console.log(displayName, email, photoURL, uid, jwt);
+
+    //       // send data to auth context/provider or jwt only
+
+    //       window.location.href = "/";
+    //     }
+    //   });
+
+    signInWithEmailAndPassword(auth, email, password).then((data) =>
+      router.push("/")
+    );
+    // console.log(data);
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -29,20 +69,9 @@ export default function Login(props) {
     } else if ((errorBlank == false) & (errorPassword == false)) {
       setSubmit(true);
     }
-    console.log(formData);
-  };
 
-  // disappear send alert after 3s
-  useEffect(() => {
-    if (submit) {
-      console.log("submit: " + submit);
-      setTimeout(() => {
-        setSubmit(false);
-        userEmail.current.value = null;
-        userPassword.current.value = null;
-      }, 3000);
-    }
-  }, [submit]);
+    postData(formData.email, formData.password);
+  };
 
   //disappear error alert after 3s
   useEffect(() => {
@@ -59,6 +88,14 @@ export default function Login(props) {
         setErrorPassword(false);
       }, 3000);
   }, [errorPassword]);
+
+  //disappear error alert after 3s
+  useEffect(() => {
+    if (serverResponse.length > 0)
+      setTimeout(() => {
+        setServerResponse("");
+      }, 3000);
+  }, [serverResponse]);
 
   return (
     <form method="post" onSubmit={submitHandler}>
@@ -79,6 +116,8 @@ export default function Login(props) {
             <p>Fill all the blanks to log in</p>
           ) : errorPassword ? (
             <p>Password length must be at least 6 characters long</p>
+          ) : serverResponse.length > 0 ? (
+            <p>{serverResponse}</p>
           ) : (
             submit && <p>Form sent successfully</p>
           )}
@@ -86,7 +125,7 @@ export default function Login(props) {
 
         <div className={styles.button_container}>
           {/* disable button for 5s after send */}
-          {submit || errorBlank || errorPassword ? (
+          {submit || errorBlank || errorPassword || serverResponse ? (
             <input type="submit" value="Log In" disabled></input>
           ) : (
             <input type="submit" value="Log In"></input>
