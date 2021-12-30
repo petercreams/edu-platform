9fa5f01d151bfe1009f0f50e959ed0d6fc2da21e
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-30 18:28:49 +0100

    style: code refactor

--- components/User/CardOption.js
+++ components/User/CardOption.js
@@ -1,18 +1,20 @@
 import styles from "./CardOption.module.scss";
 import Link from "next/dist/client/link";
 
+import { useRouter } from "next/router";
+
 export default function CardOption(props) {
+  const router = useRouter();
+
   return (
-    <Link href={props.link}>
-    <div className={styles.optionCard}>
-        <div className={styles.icon_container}>
-          <img src={props.icon} />
-        </div>
-        <h2>{props.text}</h2>
-        <div className={styles.arrow_container}>
-          <img src="/icons/arrow.svg" />
-        </div>
-      </div>  
-    </Link>
+    <div onClick={() => router.push(props.link)} className={styles.optionCard}>
+      <div className={styles.icon_container}>
+        <img src={props.icon} />
+      </div>
+      <h2>{props.text}</h2>
+      <div className={styles.arrow_container}>
+        <img src="/icons/arrow.svg" />
+      </div>
+    </div>
   );
 }
