65efda504276f7b55e2a58417372a2559ddeccb6
Piotr Smietanka <piotrsm99@gmail.com>
2021-12-30 18:30:24 +0100

    style: code refactor

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 import "../styles/global.scss";
-import { useUser } from "../firebase/useUserNode";
-import { useState, useEffect } from "react";
-import { AuthProvider } from "../firebase/AuthProvider";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { getAuth } from "@firebase/auth";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  );
+  const [user, loading, error] = useAuthState(getAuth());
+  const router = useRouter();
+
+  console.log("Loading: ", loading, " | ", "Current user: ", user);
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
